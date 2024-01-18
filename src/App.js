ffb9c8148a596281d8a733a43903880d95c0e03e
Yann Lima <yann_athayde@hotmail.com>
2024-01-17 22:40:16 -0300

    [Complete homepage and create new page for men, banners included]

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import  Shop  from './Pages/Shop.jsx';
-import { ShopCategory } from './Pages/ShopCategory';
+import ShopCategory  from './Pages/ShopCategory';
 import { Product } from './Pages/Product';
 import { Cart } from './Pages/Cart';
 import { LoginSignup } from './Pages/LoginSignup';
+import Footer from './Components/Footer/Footer.jsx';
+import men_banner from './Components/Assets/banner_mens.png'
+import women_banner from './Components/Assets/banner_women.png'
+import kid_banner from './Components/Assets/banner_kids.png'
+
+
 
 function App() {
   return (
@@ -14,15 +20,16 @@ function App() {
         <Navbar/>
         <Routes>
           <Route path='/' element={<Shop/>}/>
-          <Route path='/mens' element={<ShopCategory category="men"/>}/>
-          <Route path='/womans' element={<ShopCategory category="woman"/>}/>
-          <Route path='/kids' element={<ShopCategory category="kid"/>}/>
+          <Route path='/mens' element={<ShopCategory banner={men_banner} category="men"/>}/>
+          <Route path='/womans' element={<ShopCategory banner={women_banner} category="woman"/>}/>
+          <Route path='/kids' element={<ShopCategory banner={kid_banner} category="kid"/>}/>
           <Route path="product" element={<Product/>}>
             <Route path=':productId' element={<Product/>}/>
           </Route>
           <Route path='/cart' element={<Cart/>}/>
           <Route path='/login' element={<LoginSignup/>}/>
         </Routes>
+        <Footer/>
       </BrowserRouter>
     </div>
   );
--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import Hero from '../Components/Hero/Hero'
 import Popular from '../Components/Popular/Popular'
 import Offers from '../Components/Offers/Offers'
+import NewCollections from '../Components/NewCollections/NewCollections'
+import NewLetter from '../Components/NewsLetter/NewLetter'
+
 
 const Shop = () =>{
   return(
@@ -9,6 +12,8 @@ const Shop = () =>{
       <Hero />
       <Popular />
       <Offers />
+      <NewCollections />
+      <NewLetter />
     </div>
   )
 }
